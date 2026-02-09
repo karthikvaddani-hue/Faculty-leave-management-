0863bb4706f905f97eb54d0fd0944fb25540277f
karthikvaddani-hue <karthikvaddani@gmail.com>
2026-02-09 20:58:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function loginFaculty() {
+    alert("Faculty Login Successful");
+    window.location.href = "faculty-dashboard.html";
+}
+
+function loginAdmin() {
+    alert("Admin Login Successful");
+    window.location.href = "admin-dashboard.html";
+}
+
+function sendLeave() {
+    alert("Leave Applied Successfully!\nEmail sent to Consultant Faculty.");
+}
+
+function approve() {
+    alert("Leave Approved!\nEmail notification sent.");
+}
+
+function reject() {
+    alert("Leave Rejected!\nEmail notification sent.");
+}
